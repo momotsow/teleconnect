72839509a98181603b876bbe01e3ca576bae237d
momotsow <kgomotsomkansi@gmail.com>
2020-04-28 02:32:21 +0200

    files

--- js/readmore.js
+++ js/readmore.js
@@ -1,14 +1,24 @@
-$(document).ready(function() {
-    $("#toggle").click(function() {
-      var elem = $("#toggle").text();
-      if (elem == "Read More") {
-        //Stuff to do when btn is in the read more state
-        $("#toggle").text("Read Less");
-        $("#text").slideDown();
-      } else {
-        //Stuff to do when btn is in the read less state
-        $("#toggle").text("Read More");
-        $("#text").slideUp();
-      }
-    });
-  });
+// Add click event dynamically
+$(document).on("click", ".toggle-text-button", function() {
+
+  // Check if text is more or less
+  if ($(this).text() == "Read More") {
+
+    // Change link text
+    $(this).text("Read Less");
+    
+    // Travel up DOM tree to parent, then find any children with CLASS .toggle-text and slide down
+    $(this).parent().children(".toggle-text").slideDown();
+    
+  } else {
+
+
+    // Change link text
+    $(this).text("Read More");
+    
+    // Travel up DOM tree to parent, then find any children with CLASS .toggle-text and slide up 
+    $(this).parent().children(".toggle-text").slideUp();
+    
+  }
+  
+});
