8e9c0bc8733cbcb42bc2d850bd2185c497b2a6e9
momotsow <kgomotsomkansi@gmail.com>
2020-04-30 12:01:11 +0200

    files

--- contact_process.php
+++ contact_process.php
@@ -7,23 +7,26 @@ if(empty($_POST['name'])      ||
    empty($_POST['email'])    ||
    empty($_POST['phone'])     ||
    empty($_POST['province'])  ||
-   empty($_POST['services'])   ||
+   count($_POST['services[]']) < 0  ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
    return false;
    }
+
    
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
-$province = strip_tags(htmlspecialchars($_POST['province']));
-$message = strip_tags(htmlspecialchars($_POST['services']));
+$province = $_POST['province'];
+$selectedServices = implode(' | ', $_POST['services']);
+
    
 // Create the email and send the message
 $to = 'enquires@teleconnect.co.za'; 
-$email_subject = "Website Quote Form:  $name";
-$email_body = "You have received a new message from your website Quote form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\n\n\nServices: $servicesPhone: $phone\n\nMessage:\n$message";
+$email_subject = "Website's Expression of Interest Form:  $name";
+$email_body = "You have received a new message from your Website's Expression of Interest Form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nProvince: $province\n\n\n\nSelected Services: $selectedServices\n\n";
+
 $headers = "From: enquires@teleconnect.co.za\n";
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
